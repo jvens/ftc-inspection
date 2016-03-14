274fcae91018fb6f622f7115e7b6aa541958be63
Kris Burney <burneykb@iastate.edu>
2016-03-14 18:41:39 -0500

    updated to include inspection times

--- ftc_sql_commands.sql
+++ ftc_sql_commands.sql
@@ -35,31 +35,35 @@ robot_inspection_status ENUM('NOT_SEEN','PASS', 'FAIL') DEFAULT 'NOT_SEEN',
 field_inspection_status ENUM('NOT_SEEN','PASS', 'FAIL') DEFAULT 'NOT_SEEN',
 robot_inspection_comment VARCHAR(140),
 field_inspection_comment VARCHAR(140),
+robot_inspection_time VARCHAR(32) NOT NULL,
+field_inspection_time VARCHAR(32) NOT NULL,
 last_updated datetime 
 );
 
 
 # adding test teams to the database
-INSERT INTO team (division, team_name, team_number, last_updated)
-VALUES ("Ortberg", "Kool Kids", 12345, NOW());
+INSERT INTO team (division, team_name, team_number, robot_inspection_time, field_inspection_time, last_updated)
+VALUES ("Ortberg", "Kool Kids", 12345, "12:45pm", "12:55pm", NOW());
 
-INSERT INTO team (division, team_name, team_number, last_updated)
-VALUES ("Ryerson", "Lemons", 5466, NOW());
+INSERT INTO team (division, team_name, team_number, robot_inspection_time, field_inspection_time, last_updated)
+VALUES ("Ryerson", "Lemons", 5466, "12:35pm", "12:45pm", NOW());
 
-INSERT INTO team (division, team_name, team_number, last_updated)
-VALUES ("Ortberg", "beta", 3550, NOW());
+INSERT INTO team (division, team_name, team_number, robot_inspection_time, field_inspection_time, last_updated)
+VALUES ("Ortberg", "beta", 3550, "12:25pm", "12:30pm", NOW());
 
-INSERT INTO team (division, team_name, team_number, last_updated)
-VALUES ("Ryerson", "Even more clever of a name", 5925, NOW());
+INSERT INTO team (division, team_name, team_number, robot_inspection_time, field_inspection_time, last_updated)
+VALUES ("Ryerson", "Even more clever of a name", 5925, " 12:15pm", " 12:50pm", NOW());
 
 
 #if you have a scv file that follows this exact format then use the following script and command
 /* filename should be `teams.sql`
+lines are in the form of `team_number`,`team_name`,`division`,`robot_inspection_time`,`field_inspection_time`
 
-5466,Combustable Lemons,Ortberg
-12345,Big Boom Boom Boys,Ryerson
-00019,The teens,Ortberg
-80085,Do you get It?,Ryerson
+5466,Combustable Lemons,Ortberg,12:45pm,12:55pm
+12345,Big Boom Boom Boys,Ryerson,12:25pm,1:15pm
+00019,The teens,Ortberg,11:55pm,2:00pm
+80085,Do you get It?,Ryerson,1:25pm,1:30pm
+
 */
 
 #run ./insertScript.sh\
--- teams.sql
+++ teams.sql
@@ -1,4 +1,4 @@
-INSERT INTO team (team_number, team_name, division, last_updated) VALUES (5466,"Combustable Lemons","Ortberg", NOW());
-INSERT INTO team (team_number, team_name, division, last_updated) VALUES (12345,"Big Boom Boom Boys","Ryerson", NOW());
-INSERT INTO team (team_number, team_name, division, last_updated) VALUES (00019,"The teens"," Ortberg", NOW());
-INSERT INTO team (team_number, team_name, division, last_updated) VALUES (80085,"Do you get It?","Ryerson", NOW());
+INSERT INTO team (team_number, team_name, division, robot_inspection_time, field_inspection_time, last_updated) VALUES (5466,"Combustable Lemons","Ortberg","12:45pm","12:45pm", NOW());
+INSERT INTO team (team_number, team_name, division, robot_inspection_time, field_inspection_time, last_updated) VALUES (12345,"Big Boom Boom Boys","Ryerson","12:25pm","12:25pm", NOW());
+INSERT INTO team (team_number, team_name, division, robot_inspection_time, field_inspection_time, last_updated) VALUES (00019,"The teens","Ortberg","11:55pm","11:55pm", NOW());
+INSERT INTO team (team_number, team_name, division, robot_inspection_time, field_inspection_time, last_updated) VALUES (80085,"Do you get It?","Ryerson","1:25pm","1:25pm", NOW());
